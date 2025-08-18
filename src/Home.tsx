b494cf7d4f9de0f997b503f6c1e3c5c3ed1d0385
angelavzhu <angelzhua@gmail.com>
2025-08-17 21:08:24 -0400

    Edits to homepage

--- src/Home.tsx
+++ src/Home.tsx
@@ -26,67 +26,72 @@ export default function Home() {
       backgroundAttachment: 'fixed',
       backgroundPosition: 'center'
     }}>
-      <NavBar position="relative" />
-
-      {/* contents */}
       <Box sx={{
-        px: '32px',
-        mx: '80px',
-        my: '12px',
-        overflowY: 'scroll',
-        flexWrap: { xs: 'wrap', sm: 'wrap', md: 'nowrap', lg: 'nowrap' },
-        gap: '32px',
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'center',
         backgroundColor: 'rgba(167, 205, 228, 0.5)',
-        backdropFilter: 'blur(8px) ',
-        borderRadius: '20px',
+        backdropFilter: 'blur(4px) ',
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        overflowY: 'hidden',
       }}>
-        {/* lhs */}
+        <NavBar position="relative" />
+        {/* contents */}
         <Box sx={{
+          px: '32px',
+          mx: '80px',
+          my: '12px',
+          overflowY: 'scroll',
+          flexWrap: { xs: 'wrap', sm: 'wrap', md: 'nowrap', lg: 'nowrap' },
+          gap: '32px',
           display: 'flex',
-          alignItems: 'left',
-          justifyContent: 'space-between',
-          overflowY: 'auto',
-          position: { xs: 'relative', s: 'relative', md: 'sticky', lg: 'sticky' },
-          top: 0,
-          flexDirection: 'column',
-          marginLeft: '2%',
-          py: '40px',
-          width: { xs: '100%', s: '100%', md: '45%', lg: '45%' },
+          flexDirection: 'row',
+          justifyContent: 'center',
         }}>
-          <Stack>
-            <Typography variant="accent" fontSize="60px"> Angela Zhu,</Typography>
-            <Typography variant="accent" fontFamily="Lunasima">product designer & front-end developer</Typography>
-            <Typography my="30px" variant="heading3"> Designing intuitive and accessible products, with a touch of humanity.</Typography>
-          </Stack>
-          <Stack direction="row" gap="4px" alignItems="center">
-            <Button href={"/work"} >
-              <Typography variant="body1" fontStyle="italic" textTransform="none" textAlign="left" width="100%" color="black">
-                See All</Typography>
-            </Button>
-            <ChevronRightIcon />
-          </Stack>
+          {/* lhs */}
+          <Box sx={{
+            display: 'flex',
+            alignItems: 'left',
+            justifyContent: 'space-between',
+            overflowY: 'auto',
+            position: { xs: 'relative', s: 'relative', md: 'sticky', lg: 'sticky' },
+            top: 0,
+            flexDirection: 'column',
+            marginLeft: '2%',
+            py: '40px',
+            width: { xs: '100%', s: '100%', md: '45%', lg: '45%' },
+          }}>
+            <Stack marginBottom="20px">
+              <Typography variant="accent" fontSize="60px"> Angela Zhu,</Typography>
+              <Typography my="20px" variant="accent" fontFamily="Lunasima">product designer & front-end developer</Typography>
+              <Typography variant="heading3"> Designing intuitive and accessible products, with a touch of humanity.</Typography>
+            </Stack>
+            <Stack direction="row" gap="4px" alignItems="center">
+              <Button href={"/work"} >
+                <Typography variant="body1" fontStyle="italic" textTransform="none" textAlign="left" width="100%" color="black">
+                  See All</Typography>
+              </Button>
+              <ChevronRightIcon />
+            </Stack>
 
-        </Box>
+          </Box>
 
-        {/* rhs */}
-        <Box sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          height: '100%',
-          width: { xs: '100%', s: '100%', md: '50%', lg: '50%' },
-          gap: '24px',
-          paddingBottom: '20%'
-        }}>
-          <Thumbnail name="Evently" imgsrc={evently} date="Feb - May 2025" description="Event discovery on campus, personalized." link="/evently" />
-          <Thumbnail name="Combat Robotics @ Cornell" imgsrc={crc} date="Jun - Aug 2025" description="Modernizing the team’s website while maintaining its personality." link="/crc" />
-          <Thumbnail name="AlgoLink" imgsrc={algolink} date="May 2025 - present" description="Recruitment done right. Iterating and finalizing product designs for AlgoLink’s jobs and connections features, onboarding, and more." link="https://www.algolink.net/" />
-          <Thumbnail name="Skillful" imgsrc={skillful} date="April 2025" description="Submission to Figma 2025 Designathon (FigBuild), designed a skill trading marketplace in a team of 4 designers." link="https://www.figma.com/deck/WAB2nOFTjxApO9CxSsv9QR/FigBuild-2025--Skillful?node-id=1-130&viewport=-17%2C-67%2C0.35&t=CeJ8aAAFRKXAPclv-1&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1" />
+          {/* rhs */}
+          <Box sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            height: '100%',
+            width: { xs: '100%', s: '100%', md: '50%', lg: '50%' },
+            gap: '24px',
+            paddingBottom: '20%'
+          }}>
+            <Thumbnail name="Evently" imgsrc={evently} date="Feb - May 2025" description="Event discovery on campus, personalized." link="/evently" />
+            <Thumbnail name="Combat Robotics @ Cornell" imgsrc={crc} date="Jun - Aug 2025" description="Modernizing the team’s website while maintaining its personality." link="/crc" />
+            <Thumbnail name="AlgoLink" imgsrc={algolink} date="May 2025 - present" description="Recruitment done right. Iterating and finalizing product designs for AlgoLink’s jobs and connections features, onboarding, and more." link="https://www.algolink.net/" />
+            <Thumbnail name="Skillful" imgsrc={skillful} date="April 2025" description="Submission to Figma 2025 Designathon (FigBuild), designed a skill trading marketplace in a team of 4 designers." link="https://www.figma.com/deck/WAB2nOFTjxApO9CxSsv9QR/FigBuild-2025--Skillful?node-id=1-130&viewport=-17%2C-67%2C0.35&t=CeJ8aAAFRKXAPclv-1&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1" />
+          </Box>
         </Box>
+        <Footer color="black" />
       </Box>
-      <Footer color="black" />
 
     </Box >
   )
