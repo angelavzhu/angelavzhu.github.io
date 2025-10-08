2d8d513bcc3dcd151801ebb728727c52f3c3bcc7
angelavzhu <angelzhua@gmail.com>
2025-10-08 14:52:01 -0400

    Small updates to home page

--- src/Home.tsx
+++ src/Home.tsx
@@ -74,12 +74,12 @@ export default function Home() {
           }}>
             <Typography variant="accent" fontSize="4rem" color="#007700" lineHeight={1.5} width="100%" textAlign="center"> Angela Zhu </Typography>
             <Box textAlign="center" marginBottom="32px" marginTop="16px">
-              <Typography variant="heading1" fontSize="2rem" lineHeight={1.5}>Designing and programming inclusive experiences that fill users with </Typography>
-              <Typography variant="heading1" fontSize="2rem" fontWeight="500" color="#007700">inspiration.</Typography>
+              <Typography variant="heading1" fontSize="2rem" lineHeight={1.5}>Designer and programmer that distills bold ideas into </Typography>
+              <Typography variant="heading1" fontSize="2rem" fontWeight="500" color="#007700">concrete, scalable products.</Typography>
             </Box>
             <Box>
-              <Typography variant="heading2" color="#5B5B5B" textAlign="center" width="100%"> Currently at </Typography>
-              <Typography variant="heading2" sx={{
+              <Typography variant="heading3" color="#5B5B5B" textAlign="center" width="100%"> Currently designing experiences for </Typography>
+              <Typography variant="heading3" sx={{
                 fontWeight: 'bold',
                 '&:hover': {
                   background: 'linear-gradient(to right, #4285F4 0% 20%, #EA4335 20% 40%, #FAC10B 40% 55%, #4285F4 55% 75%, #34A853 66.67% 83.33%, #EA4335 83.33% 100%)',
@@ -89,7 +89,7 @@ export default function Home() {
                   display: 'inline-block',
                 }
               }}>Google</Typography>
-              <Typography variant="heading2" color="#5B5B5B" >, previously at multiple startups.</Typography>
+              <Typography variant="heading3" color="#5B5B5B" >, previously at multiple startups.</Typography>
 
             </Box>
           </Stack>
@@ -128,9 +128,9 @@ export default function Home() {
             alignItems: 'center',
             justifyContent: 'center'
           }}>
-          <Thumbnail name="Google" mainTag="Design Consultant (Contract)" tags="/ Mobile / UXR" color="#000000" imgsrc={google} description="[In Progress] Designing and evaluating user experiences to engage college-age users with Google products. " link="/google" />
           <Thumbnail name="Evallos (Formerly AlgoLink)" mainTag="Design Internship" tags="/ Desktop / Startup" color="#003DF5" imgsrc={algolink} description="[Under NDA] Owned 6 core product features including job boards, messaging, onboarding, and more. Scaled the platform by introducing evaluation and improvement services for AI models. " link="/algolink" />
           <Thumbnail name="Evently" mainTag="Design Consulting at Cornell" tags="/ Mobile / 0 -> 1 Design" color="#FF6F8D" imgsrc={evently} description="Prototyped a mobile app to streamline the event discovery process at Cornell with a focus on community and lasting connection." link="/evently" rotate="-2deg" />
+          <Thumbnail name="Google" mainTag="Design Consultant (Contract)" tags="/ Mobile / UXR" color="#000000" imgsrc={google} description="[In Progress] Designing and evaluating user experiences to engage college-age users with Google products. " link="/google" />
           <Thumbnail name="Combat Robotics @ Cornell" mainTag="Website Redesign" tags="/ Desktop & Mobile / Design Systems" color="#B21D1D" imgsrc={crc} description="Led a full website redesign including information architecture, site structure, and visual branding to create more intuitive navigation and attract more applicants." link="/crc" rotate="2deg" />
           <Thumbnail name="Skillful" mainTag="Design-a-thon" tags="/ Mobile / Speculative Design" color="#111C6B" imgsrc={skillful} description="Designed a skill-trading marketplace for a world with superpowers in a team of 4 designers for the Figma 2025 design-a-thon." link="https://www.figma.com/deck/WAB2nOFTjxApO9CxSsv9QR/FigBuild-2025--Skillful?node-id=1-130&viewport=-17%2C-67%2C0.35&t=CeJ8aAAFRKXAPclv-1&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1" rotate="-2deg" external="True" />
         </Box>
