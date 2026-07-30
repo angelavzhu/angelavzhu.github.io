a0f303ab3112fb4c35c6607cdcea1f47e7ccbfc6
angelavzhu <angelzhua@gmail.com>
2026-07-29 21:39:51 -0400

    Add build information to config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true, // required for static export; next/image needs a server otherwise
+  },
 };
+
+module.exports = nextConfig;
 
 export default nextConfig;
